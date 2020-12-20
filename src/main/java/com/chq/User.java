e33ffab1dcdd58e44761e11efc5dba79e06f564f
chq <972838994@qq.com>
2020-12-20 19:41:32 +0800

    test

---
+++ src/main/java/com/chq/User.java
@@ -0,0 +1,22 @@
+package com.chq;
+
+/**
+ * 项目名称:  gitdemo
+ * 包:        com.chq
+ * 类名称:    User
+ * 类描述:
+ * 创建人:    chq
+ * 创建时间:  2020/12/20 19:36
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
